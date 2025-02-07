1acfcd20b164038c50f5604f914d8f2dfc82e520
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-02-08 00:05:07 +0300

    add navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 
 // Определяем компонент NavBar
 const NavBar: React.FC = () => {
-  // Состояние для управления отображением меню
-
   return (
     <div>
       <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
@@ -13,27 +11,37 @@ const NavBar: React.FC = () => {
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               {/* Логотип */}
               <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold">tolmach-blog</h2>
+                <h2 className="text-2xl text-cyan-600 font-bold">
+                  [tolmach-blog]
+                </h2>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <div>
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold pr-4">[about]</h2>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold pr-4">[blog]</h2>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold pr -4">[articles]</h2>
               </Link>
             </div>
           </div>
           {/* Контейнер меню */}
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0`}
-            >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex">
-                {/* Пункты меню */}
-              </ul>
-            </div>
-          </div>
         </div>
       </nav>
     </div>
   );
 };
 
-console.log("NavBar:", NavBar);
+console.log("NavBar sssss:", NavBar);
 
 // это чтобы можно было экспортировать компоненту
 export default NavBar;
