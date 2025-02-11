33f7bd7329b6f73b14030c509a83013cbbb48377
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-02-11 23:27:08 +0300

    trying add useRouter

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,40 +1,41 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+ 
+import { useRouter } from 'next/navigation'
 
 // Определяем компонент NavBar
 const NavBar: React.FC = () => {
+  const router = useRouter();
+  console.log("abobus ", router);
   return (
     <div>
-        {/* fixed делает навигационную панель фиксированной относительно скроллингга */}
+      {/* fixed делает навигационную панель фиксированной относительно скроллингга */}
       <nav className="bg-black fixed top-0 left-0 right-0 z-10">
         {/* justify-around равонмерное распределение с отступами по краям */}
         {/* px-10  равонмерное распределение с отступами по краям */}
         {/* flex  если flex не использовать то блочные элементы (div) будут идти столбиком  */}
         <div className="flex justify-around py-5">
           <div>
-            <div className="">
-              {/* Логотип */}
-              <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold">
-                  [tolmach-blog]
-                </h2>
-              </Link>
-            </div>
+            {/* Логотип */}
+            <Link href="/">
+              <h2 className="text-2xl text-cyan-600">[tolmach-blog]</h2>
+            </Link>
           </div>
           <div className="flex flex-row">
             <div>
-              <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold pr-4">[about]</h2>
+              <Link href="/about">
+                <h2 className="text-2xl text-cyan-600 pr-4">[about]</h2>
               </Link>
             </div>
             <div>
-              <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold pr-4">[blog]</h2>
+              <Link href="/blog">
+                <h2 className="text-2xl text-cyan-600 pr-4">[blog]</h2>
               </Link>
             </div>
             <div>
-              <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold pr -4">[articles]</h2>
+              <Link href="/articles">
+                <h2 className="text-2xl text-cyan-600 pr -4">[articles]</h2>
               </Link>
             </div>
           </div>
@@ -44,8 +45,6 @@ const NavBar: React.FC = () => {
     </div>
   );
 };
-
-console.log("NavBar sssss:", NavBar);
 
 // это чтобы можно было экспортировать компоненту
 export default NavBar;
