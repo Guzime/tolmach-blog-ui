46b0f5333049f081c6c956cf0ff3afe798bf50e8
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-02-26 23:59:09 +0300

    add experience component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <div className="flex justify-center gap-6">
         <AboutMe />
         <StackMe />
-        <ExperienceMe />
       </div>
+      <ExperienceMe />
     </div>
   );
 }
--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,8 +1,21 @@
 const ExperienceMe: React.FC = () => {
   return (
-    <div className="flex justify-center mt-20">
+    <div className="flex justify-center mt-6">
       <div className="bg-white p-8 rounded-xl  border border-customBorder flex flex-col items-center ">
-      <h1 className="text-lg font-sans text-black">Experience</h1>
+        <h1 className="text-lg font-sans text-black">Experience</h1>
+        <div className="flex justify-center gap-6">
+          <div className="flex gap-1 flex-row p-0.5">
+            <img
+              src="calendar.svg"
+              alt="icon"
+              className="w-5 h-5 object-contain"
+            />
+            <p className="font-sans text-[#9198A1]">2021-2024</p>
+          </div>
+          <div>
+          <h1 className="text-lg font-sans text-black">Team Lead & Developer</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
