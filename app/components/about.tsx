28db5a51e284a4ac37b3b2b4cd1eb8e85d528430
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-03-05 23:26:36 +0300

    add new component contact

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -3,51 +3,52 @@ import Contact from "./contact";
 
 const AboutMe: React.FC = () => {
   return (
-    <div>
-    <div className="flex justify-center items-center mt-20">
-      <div className="bg-white p-8 rounded-xl  border border-customBorder flex flex-col items-center ">
-        <Image
-          src="https://avatars.githubusercontent.com/u/40675616?v=4"
-          alt="Картинка прикола"
-          width={200}
-          height={200}
-          className="rounded-full mb-4 "
-        />
-        <div className="flex flex-col items-start">
-          <h1 className="text-lg text-black font-sans"> Tolmachev Nikita</h1>
-          <div className="flex gap-1">
-            <Image
-              width={5}
-              height={5}
-              src="badge.svg"
-              alt="icon"
-              className="w-5 h-5 object-contain"
-            />
-            <p className="font-sans text-[#9198A1]">Java Developer</p>
-          </div>
-          <div className="flex gap-1">
-            <Image
-              width={5}
-              height={5}
-              src="location.svg"
-              alt="icon"
-              className="w-5 h-5 object-contain"
-            />
-            <p className="font-sans  text-[#9198A1]">Russia, Moscow</p>
-          </div>
-          <div className="flex gap-1">
-            <Image
-              width={5}
-              height={5} 
-              src="work.svg" 
-              alt="icon" 
-              className="w-5 h-5 object-contain" />
-            <p className="font-sans text-[#9198A1]">Diasoft</p>
+    <div className="w-[280]">
+      <div className="flex justify-center items-center mt-20">
+        <div className="bg-white p-8 rounded-xl  border border-customBorder flex flex-col items-center w-full">
+          <Image
+            src="https://avatars.githubusercontent.com/u/40675616?v=4"
+            alt="Картинка прикола"
+            width={200}
+            height={200}
+            className="rounded-full mb-4 "
+          />
+          <div className="flex flex-col items-start">
+            <h1 className="text-lg text-black font-sans"> Tolmachev Nikita</h1>
+            <div className="flex gap-1">
+              <Image
+                width={5}
+                height={5}
+                src="badge.svg"
+                alt="icon"
+                className="w-5 h-5 object-contain"
+              />
+              <p className="font-sans text-[#9198A1]">Java Developer</p>
+            </div>
+            <div className="flex gap-1">
+              <Image
+                width={5}
+                height={5}
+                src="location.svg"
+                alt="icon"
+                className="w-5 h-5 object-contain"
+              />
+              <p className="font-sans  text-[#9198A1]">Russia, Moscow</p>
+            </div>
+            <div className="flex gap-1">
+              <Image
+                width={5}
+                height={5}
+                src="work.svg"
+                alt="icon"
+                className="w-5 h-5 object-contain"
+              />
+              <p className="font-sans text-[#9198A1]">Diasoft</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-      <Contact/>
+      <Contact />
     </div>
   );
 };
--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,9 +1,44 @@
+import Image from "next/image";
+
 const Contact: React.FC = () => {
-    return(
-        <h1>
-            АБОБСУСУСУС
-        </h1>
-    );
+  return (
+    <div className="flex mt-6">
+      <div className="flex flex-col w-full">
+        <div className="ml-10 flex flex-col ">
+          <div className="flex gap-1">
+            <Image
+              width={5}
+              height={5}
+              src="link.svg"
+              alt="icon"
+              className="w-5 h-5 object-contain mt-0.5"
+            />
+            <p className="font-sans text-[#9198A1]">Mail</p>
+          </div>
+          <div className="flex gap-1">
+            <Image
+              width={5}
+              height={5}
+              src="link.svg"
+              alt="icon"
+              className="w-5 h-5 object-contain mt-0.5"
+            />
+            <p className="font-sans  text-[#9198A1]">Telegram</p>
+          </div>
+          <div className="flex gap-1">
+            <Image
+              width={5}
+              height={5}
+              src="link.svg"
+              alt="icon"
+              className="w-5 h-5 object-contain mt-0.5"
+            />
+            <p className="font-sans text-[#9198A1]">GitHub</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
