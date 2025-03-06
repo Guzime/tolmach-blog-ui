12b43d0b3227ab7017815105be86ee5fdf74553c
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-03-06 23:43:01 +0300

    hard set high about component

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -5,7 +5,7 @@ const AboutMe: React.FC = () => {
   return (
     <div className="w-[280]">
       <div className="flex justify-center items-center mt-20">
-        <div className="bg-white p-8 rounded-xl  border border-customBorder flex flex-col items-center w-full">
+        <div className="bg-white p-8 rounded-xl  border border-customBorder flex flex-col items-center w-full h-[400]">
           <Image
             src="https://avatars.githubusercontent.com/u/40675616?v=4"
             alt="Картинка прикола"
