baa69793facab276854ce91da3caa41004f8ba3b
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-03-13 00:50:44 +0300

    edit style

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -33,22 +33,22 @@ const NavBar: React.FC = () => {
           <div className="flex flex-row">
             <div onClick={() => setActive('/about')}>
               <Link href="/about">
-                <h2 className={`text-2xl pr-4 font-sans ${active === '/about' ? 'text-[#DCD7C9]' : 'text-black'}`}>About</h2>
+                <h2 className={`hover:opacity-60 transform transition duration-200 text-2xl pr-4 font-sans ${active === '/about' ? 'text-[#DCD7C9]' : 'text-black'}`}>About</h2>
               </Link>
             </div>
             <div>
               <Link href="/blog">
-              <h2 className={`text-2xl pr-4 font-sans ${active === '/blog' ? 'text-[#DCD7C9]' : 'text-black'}`}>Articles</h2>
+              <h2 className={`hover:opacity-60 transform transition duration-200 text-2xl pr-4 font-sans ${active === '/blog' ? 'text-[#DCD7C9]' : 'text-black'}`}>Articles</h2>
               </Link>
             </div>
             <div>
               <Link href="/articles">
-              <h2 className={`text-2xl pr-4 font-sans ${active === '/articles' ? 'text-[#DCD7C9] ' : 'text-black'}`}>Blog</h2>
+              <h2 className={`hover:opacity-60 transform transition duration-200 text-2xl pr-4 font-sans ${active === '/articles' ? 'text-[#DCD7C9] ' : 'text-black'}`}>Blog</h2>
               </Link>
             </div>
             <div>
               <Link href="/knowledge">
-              <h2 className={`text-2xl pr-4 font-sans ${active === '/knowledge' ? 'text-[#DCD7C9] ' : 'text-black'}`}>Knowledge base</h2>
+              <h2 className={`hover:opacity-60 transform transition duration-200 text-2xl pr-4 font-sans ${active === '/knowledge' ? 'text-[#DCD7C9] ' : 'text-black'}`}>Knowledge base</h2>
               </Link>
             </div>
           </div>
