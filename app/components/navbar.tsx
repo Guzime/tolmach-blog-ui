3623378a26602620e663bf6cc8a34c44c89a5766
Толмачев Никита Александрович <forblachok@yandex.ru>
2025-03-15 01:13:02 +0300

    add button login

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -49,7 +49,9 @@ const NavBar: React.FC = () => {
               </Link>
             </div>
           </div>
-          {/* Контейнер меню */}
+          <button className="px-6 py-2 bg-blue-600 text-white font-sans rounded-lg shadow-md hover:opacity-80 transform transition duration-200 focus:outline-none focus:ring-2 focus:opacity-80">
+            Log In
+          </button>
         </div>
       </nav>
     </div>
